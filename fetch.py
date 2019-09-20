fda28369d30d8ef9dd6b26e2c4f248c2f5ca5b4d
Konstantin Goncharov <konstantin.goncharov@inplat.ru>
2019-09-20 18:25:30 +0300

    Fetches

--- fetch.py
+++ fetch.py
@@ -1,13 +1,14 @@
+import asyncio
 import logging
 from urllib import request
 
 from lib import Timer
-from defs import URL
+from defs import URL, MAX_CLIENTS
 from req import aiohttp_fetch
 
 
 def fetch_sync(pid):
-    logging.info("Sync fetch started: #{pid}")
+    logging.info(f"Sync fetch started: #{pid}")
     timer = Timer()
     response = request.urlopen(URL)
     datetime_hdr = response.getheader("Date")
@@ -17,13 +18,30 @@ def fetch_sync(pid):
     return datetime_hdr
 
 
+def run_fetches_sync():
+    timer = Timer()
+    for pid in range(1, MAX_CLIENTS + 1):
+        fetch_sync(pid)
+    logging.info(f"Fetch sync process took: {timer.passed()}")
+
+
 async def fetch_a_sync(pid):
-    logging.info("A-sync fetch started: #{pid}")
+    logging.info(f"A-sync fetch started: #{pid}")
     timer = Timer()
     response = await aiohttp_fetch(URL)
     datetime_hdr = response.headers.get("Date")
 
-    logging.info(f"A-sync fetch #{pid}: {datetime_hdr}, took: {timer.passed:.2f} seconds")
+    logging.info(f"A-sync fetch #{pid}: {datetime_hdr}, took: {timer.passed():.2f} seconds")
     response.close()
 
     return datetime_hdr
+
+
+async def run_fetches_a_sync():
+    timer = Timer()
+    tasks = [
+        fetch_a_sync(pid)
+        for pid in range(1, MAX_CLIENTS + 1)
+    ]
+    await asyncio.wait(tasks)
+    logging.info(f"Fetch A-sync process took: {timer.passed()}")
--- up.py
+++ up.py
@@ -1,12 +1,21 @@
 import asyncio
+import logging
 
 from a_sync import run_sync_a_sync
+from fetch import run_fetches_a_sync, run_fetches_sync
 from sleepers import run_sleepers
 
 
+logging.getLogger().setLevel(logging.INFO)
+logging.basicConfig()
+logging.info("Started")
+
+
 async def cli():
-    await run_sleepers()
+    # await run_sleepers()
+    await run_fetches_a_sync()
 
 
 asyncio.run(cli())
-run_sync_a_sync()
+# run_sync_a_sync()
+run_fetches_sync()
